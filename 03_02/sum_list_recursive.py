f4dcbd8c18b3e0d6f3b2d3293b7c8504d05cad87
khan99-coder <khan.kkn9@gmail.com>
2023-05-23 09:58:48 +0300

    sum list recursion

--- 03_02/sum_list_recursive.py
+++ 03_02/sum_list_recursive.py
@@ -5,11 +5,20 @@ Robin Andrews - https://compucademy.net/
 
 
 def list_sum(a_list):
-    pass
+    # iterative solution
+    result = 0
+    for item in a_list:
+        result += item
+    return result
 
 
 def list_sum_recursive(a_list):
-    pass
+    # base case
+    if len(a_list) == 0:
+        return 0
+    # recursive case
+    else:
+        return a_list[0] + list_sum_recursive(a_list[1:])
 
 
 assert list_sum([2, 3, 5, 7]) == 17
