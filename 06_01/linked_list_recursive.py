4c293ca85de0fbf944249d9b10a596b9ca190a11
khan99-coder <khan.kkn9@gmail.com>
2023-05-24 11:25:03 +0300

    Linkedlist Traversal

--- 06_01/linked_list_recursive.py
+++ 06_01/linked_list_recursive.py
@@ -5,4 +5,29 @@ Robin Andrews - https://compucademy.net/
 
 
 class Node:
-    pass
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
+def traverse(head):
+    # Base case
+    if head is None:
+        return
+    # Recursive case
+    print(head.data)
+    traverse(head.next)
+    return        
+        
+        
+item1 = Node("Dog")
+item2 = Node("Cat")
+item3 = Node("Fish")
+item1.next = item2
+item2.next = item3
+
+
+# traverse(item1)
+
+head = Node("Dog", Node("Cat", Node("Fish", None)))
+traverse(head)
